8ec6cffaa98efd9a14e945715529d64237901f00
anshu0202 <anshu.verma62074@gmail.com>
2023-02-28 17:19:28 +0530

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter, Routes, Route
+} from "react-router-dom";
+
+import Home from './components/Home';
+
+
+function App() {
+  return (<BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+    
+      <Route path=""/>
+    </Routes>
+  </BrowserRouter>
+    
+  );
+}
+
+export default App;

7d3048bcf8716d7bb17ef311ddf40a769970c8b2
anshu0202 <anshu.verma62074@gmail.com>
2023-03-12 23:14:06 +0530

    authentication of user is compleeted with backend setup

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -3,10 +3,12 @@ import User from "../model/user-schema.js";
 
 export const userSignup= async(req, res)=>{
     try{
-        //     const exist= await User.findOne({username:req.body.username});
-        // if(exist){
-        //     return res.status(401).json({message:'Username already exists'});
-        // }
+
+        console.log("data from frontyend is ", req.body);        
+        const exist= await User.findOne({email:req.body.email});
+        if(exist){
+            return res.status(401).json({message:'Email already exists'});
+        }
         const user=req.body;
         console.log("user is ",user);
         const newUser= new User(user);
@@ -21,10 +23,12 @@ export const userSignup= async(req, res)=>{
 
 export const userLogin= async(req, res)=>{
         try{
-            const username=req.body.username;
+            console.log("data from front is ",req.body);
+            const email=req.body.fullname;
             const password=req.body.password;
-            const user=User.findOne({username:req.body.username, password:req.body.password});
-            
+            const user= await User.findOne({fullname:req.body.fullname, password:req.body.password});
+
+            console.log("user is ",user);
             if(user){
                return  res.status(200).json({data:user});
 
@@ -34,14 +38,69 @@ export const userLogin= async(req, res)=>{
             }
         }
         catch(error){
-                    console.log("error while login")
+                    console.log("error while login ",error.message )
                     res.status(500).json('Error ', error.message);
         }
-
-
-
 }
 
+export const verifyEmail= async(req, res)=>{
+    console.log("email is ", req.body);
+
+    try{
+
+        const email=await User.findOne({email:req.body.email});
+        console.log("email is ", email);
+        if(email){
+            return  res.status(200).json({data:email});
+        }
+        else{
+            return  res.status(401).json('Invalid email')
+        }
+    }
+
+    catch(error){
+        console.log("error while email verification ",error.message )
+        res.status(500).json('Error ', error.message);
+
+    }
+}
+
+
+
+export const updatePassword=async(req, res)=>{
+        console.log("req is ", req.body);
+        try{
+
+            const res=await User.findOne({email:req.body.email});
+            console.log("Ress update is is ", res);
+
+            res.password=req.body.password
+              await res.save();
+
+            // let result= await User.updateOne(
+            //     {email:req.body.email},
+            //     {
+            //         $set:req.body.password
+            //     }
+            //         )
+                console.log("result is ",result)
+        // const res=await User.findOne({email:req.body.email});        
+            // res.send(result)
+
+
+
+            if(res){
+                return res.status(200).json({data:res});
+            }
+                else{
+                    return res.status(401).json('something Invalid')
+                }
+        }
+        catch(error){
+            res.status(500).json("Invaild operation");
+        }
+
+} 
 
 
 
